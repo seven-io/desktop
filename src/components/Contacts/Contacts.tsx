bee521ba89ed5d9a18a6d76fcc1860bb5118e5ea
André Matthies <matthiez@gmail.com>
2025-03-17 14:19:48 +0100

    add rcs action to contacts list

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -12,13 +12,14 @@ import {cleanPhone} from '../../util/cleanPhone'
 import {initClient} from '../../util/initClient'
 import {notify} from '../../util/notify'
 import {SET_BACKDROP} from '../../store/features/backdrop'
-import {SET_TO} from '../../store/features/to'
+import {SET_TO, SET_TO_RCS} from '../../store/features/to'
 import {SET_NAV} from '../../store/features/nav'
 import Box from '@mui/material/Box'
 import {Contact, ContactsResource} from '@seven.io/client'
 import localStore from '../../util/LocalStore'
 import SmsIcon from '@mui/icons-material/Sms'
 import VoiceIcon from '@mui/icons-material/PermPhoneMsg'
+import ContactMailIcon from '@mui/icons-material/ContactMail'
 
 export const Contacts = () => {
     const {t} = useTranslation('contacts')
@@ -102,6 +103,14 @@ export const Contacts = () => {
                                     dispatch(SET_NAV('voice'))
                                 }}
                             ><VoiceIcon/></Button>
+
+                            <Button
+                                onClick={() => {
+                                    dispatch(SET_TO_RCS(contact.properties.mobile_number!))
+
+                                    dispatch(SET_NAV('rcs'))
+                                }}
+                            ><ContactMailIcon/></Button>
                         </TableCell>
                     </TableRow>)}
                 </TableBody>
