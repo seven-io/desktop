a51f614620cbbec1bb1b91ddfc671d08ec37b7a6
André Matthies <matthiez@gmail.com>
2025-08-18 08:13:59 +0200

    fix notifications z-index

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -23,7 +23,7 @@ export default function Snackbar({index, message, title}: {
         <>
             <div
                 aria-live="assertive"
-                className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+                className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-50"
             >
                 <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
                     <Transition show={show}>
